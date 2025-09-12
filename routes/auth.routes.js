168a519d6d17222fd503f683637a046b7b8223a1
Ifajul Islam <135067157+ifajulislam@users.noreply.github.com>
2025-09-13 04:20:29 +0600

    created routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const authRouter = Router();
+
+authRouter.post("/sign-up", (req, res) => res.send({ message: "Sign Up" }));
+
+authRouter.post("/sign-in", (req, res) => res.send({ message: "Sign Up" }));
+
+authRouter.post("/sign-out", (req, res) => res.send({ message: "Sign Up" }));
+
+export default authRouter;
---
+++ routes/subscripiton.routes.js
@@ -0,0 +1,37 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get("/", (req, res) =>
+  res.send({ message: "GET All Subscriptions" })
+);
+
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send({ message: "GET Subscription Details" })
+);
+
+subscriptionRouter.post("/", (req, res) =>
+  res.send({ message: "CREATE Subscription" })
+);
+
+subscriptionRouter.put("/:id", (req, res) =>
+  res.send({ message: "UPDATE Subscription" })
+);
+
+subscriptionRouter.delete("/:id", (req, res) =>
+  res.send({ message: "DELETE Subscription" })
+);
+
+subscriptionRouter.get("/user/:id", (req, res) =>
+  res.send({ message: "Get User All Subscription" })
+);
+
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send({ message: "Cancel Subscription" })
+);
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) =>
+  res.send({ message: "DELETE Subscription" })
+);
+
+export default subscriptionRouter;
