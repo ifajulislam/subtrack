4d88c2380c188c58ceaa4c7bbcbdce4e15216666
Ifajul Islam <135067157+ifajulislam@users.noreply.github.com>
2025-09-13 05:12:12 +0600

    made user model

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      match: [/\s+@\S+\. \S+/, "Please fill a valid email address"],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
